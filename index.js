cc7bdee9c2edd270d37476b563f7e0e7d574e908
Kelly Cruz <kellycruz@Kellys-MacBook-Pro.local>
2023-08-10 17:19:55 -0300

    Hello World

---
+++ index.js
@@ -0,0 +1,4 @@
+function helloWorld(name) {
+    return "Ol\u00E1 ".concat(name);
+}
+console.log(helloWorld('Mundo!!!'));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function helloWorld(name){
+  return `Olá ${name}`;
+}
+
+console.log(helloWorld('Mundo!!!'));
